93e451591bdb9369dcba10664ab8db33f3e049a2
FJ <qq1084472249@gmail.com>
2023-04-28 15:50:54 +0800

    Volatile

--- src/main/java/com/freeloop/juc/volatiles/VolatileSeeDemo.java
+++ src/main/java/com/freeloop/juc/volatiles/VolatileSeeDemo.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
  * @author fj
  * @since 2023/4/28 12:06
  */
-public class gitSeeDemo {
+public class VolatileSeeDemo {
     //main线程修改flag=false后，t1线程读取到的flag仍然为true，t1线程没有停止
     //main线程修改flag没有将其刷新到主内存，所以t1线程看不到
     //main线程将flag刷新到了主内存，但是t1线程一直读取的是自己线程工作内存中的值，没有及时去内存中获取flag的最新值
